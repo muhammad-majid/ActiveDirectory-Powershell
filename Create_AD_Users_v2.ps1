368b5ea7223bfc21e7ac4f2d09c339f62c1e78a7
muhammad-majid <42956457+muhammad-majid@users.noreply.github.com>
2018-09-09 01:47:34 +1000

    replicate gp memberships, improved logging

---
+++ Create_AD_Users_v2.ps1
@@ -0,0 +1,201 @@
+###########################################################
+# AUTHOR  : Muhammad Majid
+# DATE    : 26-04-2012
+# EDIT    : 07-09-2018
+# COMMENT : This script creates new Active Directory users,
+#           including different kind of properties, based
+#           on an input_create_ad_users.csv.
+# VERSION : 1.4
+###########################################################
+##
+# CHANGELOG
+# Version 1.2: 15-04-2014 - Changed the code for better
+# - Added better Error Handling and Reporting.
+# - Changed input file with more logical headers.
+# - Added functionality for account Enabled,
+#   PasswordNeverExpires, ProfilePath, ScriptPath,
+#   HomeDirectory and HomeDrive
+# - Added the option to move every user to a different OU.
+# Version 1.3: 08-07-2014
+# - Added functionality for ProxyAddresses
+# Version 1.4: 07-09-2018
+# - Added functionality to copy user from existing user
+# Version 2: 08-09-2018
+# - Removed copy user, replicating group memberships instead.
+# - Simplified if/else and process flow
+# - Improved log output
+
+
+# ERROR REPORTING ALL
+Set-StrictMode -Version latest
+
+#----------------------------------------------------------
+# LOAD ASSEMBLIES AND MODULES
+#----------------------------------------------------------
+Try
+{
+  Import-Module ActiveDirectory -ErrorAction Stop
+}
+Catch
+{
+  Write-Host "[ERROR]`t ActiveDirectory Module couldn't be loaded. Script will stop!"
+  Exit 1
+}
+
+#----------------------------------------------------------
+#STATIC VARIABLES
+#----------------------------------------------------------
+$path     = Split-Path -parent $MyInvocation.MyCommand.Definition
+$newpath  = $path + "\import_create_ad_users.csv"
+$log      = $path + "\create_ad_users.log"
+$date     = Get-Date
+$addn     = (Get-ADDomain).DistinguishedName
+$dnsroot  = (Get-ADDomain).DNSRoot
+$i        = 1
+
+#----------------------------------------------------------
+#START FUNCTIONS
+#----------------------------------------------------------
+Function Start-Commands
+{
+  Create-Users
+}
+
+Function insertTimeStamp { return '[' + (Get-Date).ToString('yyyy-MM-dd HH:mm:ss') + '] : ' }
+
+Function Create-Users
+{
+  (insertTimeStamp) + "Processing started.." | Out-File $log -append
+  "--------------------------------------------" | Out-File $log -append
+  Import-CSV $newpath | ForEach-Object {
+  
+	Write-Host "Running iteration $i`r`n"
+	(insertTimeStamp) + "Running iteration $i.." | Out-File $log -append
+	
+	If (($_.GivenName -eq "") -Or ($_.LastName -eq ""))
+	{
+		Write-Host "[ERROR]`t Please provide valid GivenName and LastName. Processing skipped for line $($i)`r`n"
+        (insertTimeStamp) + "[ERROR]`t Please provide valid GivenName, LastName. Processing skipped for line $($i)`r`n" | Out-File $log -append
+		$i++
+		return
+	}
+    
+        # Set the target OU
+        #$location = $_.TargetOU + ",$($addn)"
+		$location = $_.TargetOU
+
+        # Set the Enabled and PasswordNeverExpires properties
+        If (($_.Enabled.ToLower()) -eq "true") { $enabled = $True } Else { $enabled = $False }
+        If (($_.PasswordNeverExpires.ToLower()) -eq "true") { $expires = $True } Else { $expires = $False }
+
+        # A check for the country, because those were full names and need
+        # to be land codes in order for AD to accept them. I used Netherlands
+        # as example
+        If($_.Country -eq "Netherlands") {$_.Country = "NL"} Else { $_.Country = "EN" }
+		
+        # Replace dots / points (.) in names, because AD will error when a
+        # name ends with a dot (and it looks cleaner as well)
+        $replace = $_.Lastname.Replace(".","")
+		
+		Write-Host "Applying naming convention to get username to be created..`r`n"
+		(insertTimeStamp) + "Applying naming convention to get username to be created.." | Out-File $log -append
+				
+        # Create sAMAccountName according to this 'naming convention':
+        # <FirstLetterInitials><FirstFourLettersLastName> for example
+        #$sam = $_.Initials.substring(0,1).ToLower() + $lastname.ToLower()
+        $sam = $_.GivenName.ToLower() + "." + $_.LastName.ToLower()
+        Try   { $exists = Get-ADUser -LDAPFilter "(sAMAccountName=$sam)" }
+        Catch {	}
+		
+		If($exists)
+		{
+			Write-Host "[SKIP]`t User $($sam) ($($_.GivenName) $($_.LastName)) already exists or returned an error!`r`n"
+			(insertTimeStamp) + "[SKIP]`t User $($sam) ($($_.GivenName) $($_.LastName)) already exists or returned an error!" | Out-File $log -append
+			$i++
+			return
+		}
+		
+          # Set all variables according to the table names in the Excel
+          # sheet / import CSV. The names can differ in every project, but
+          # if the names change, make sure to change it below as well.
+          $setpass = ConvertTo-SecureString -AsPlainText $_.Password -force
+
+          Try
+          {
+            Write-Host "[INFO]`t Creating user : $($sam)"
+            (insertTimeStamp) + "[INFO]`t Creating user : $($sam)" | Out-File $log -append
+            New-ADUser $sam -GivenName $_.GivenName -Initials $_.Initials `
+            -Surname $_.LastName -DisplayName ($_.GivenName + " " + $_.LastName) `
+            -Office $_.OfficeName -Description $_.Description -EmailAddress $_.Mail `
+            -StreetAddress $_.StreetAddress -City $_.City -State $_.State `
+            -PostalCode $_.PostalCode -Country $_.Country -UserPrincipalName ($sam + "@" + $dnsroot) `
+            -Company $_.Company -Department $_.Department -EmployeeID $_.EmployeeID `
+            -Title $_.Title -OfficePhone $_.Phone -AccountPassword $setpass -Manager $_.Manager `
+            -profilePath $_.ProfilePath -scriptPath $_.ScriptPath -homeDirectory $_.HomeDirectory `
+            -homeDrive $_.homeDrive -Enabled $enabled -PasswordNeverExpires $expires
+            Write-Host "[INFO]`t Created new user : $($sam)"
+            (insertTimeStamp) + "[INFO]`t Created new user : $($sam)" | Out-File $log -append
+
+            $dn = (Get-ADUser $sam).DistinguishedName
+            # Set an ExtensionAttribute
+            If ($_.ExtensionAttribute1 -ne "" -And $_.ExtensionAttribute1 -ne $Null)
+            {
+              $ext = [ADSI]"LDAP://$dn"
+              $ext.Put("extensionAttribute1", $_.ExtensionAttribute1)
+              Try   { $ext.SetInfo() }
+              Catch { Write-Host "[ERROR]`t Couldn't set the Extension Attribute : $($_.Exception.Message)" }
+            }
+
+            # Set ProxyAdresses
+            If ($_.proxyAddresses -ne "")
+            {
+              Try { $dn | Set-ADUser -Add @{proxyAddresses = ($_.ProxyAddresses -split ";")} -ErrorAction Stop }
+              Catch { Write-Host "[ERROR]`t Couldn't set the ProxyAddresses Attributes : $($_.Exception.Message)" }
+            }
+
+            # Move the user to the OU ($location) you set above. If you don't
+            # want to move the user(s) and just create them in the global Users
+            # OU, comment the string below
+            If ([adsi]::Exists("LDAP://$($location)"))
+            {
+              Move-ADObject -Identity $dn -TargetPath $location
+              Write-Host "[INFO]`t User $sam moved to target OU : $($location)"
+              (insertTimeStamp) + "[INFO]`t User $sam moved to target OU : $($location)" | Out-File $log -append
+            }
+            Else
+            {
+              Write-Host "[ERROR]`t Targeted OU couldn't be found. Newly created user wasn't moved!"
+              (insertTimeStamp) + "[ERROR]`t Targeted OU couldn't be found. Newly created user wasn't moved!" | Out-File $log -append
+            }
+
+            # Rename the object to a good looking name (otherwise you see
+            # the 'ugly' shortened sAMAccountNames as a name in AD. This
+            # can't be set right away (as sAMAccountName) due to the 20
+            # character restriction
+            $newdn = (Get-ADUser $sam).DistinguishedName
+            Rename-ADObject -Identity $newdn -NewName ($_.GivenName + " " + $_.LastName)
+            Write-Host "[INFO]`t Renamed $($sam) to $($_.GivenName) $($_.LastName)`r`n"
+            (insertTimeStamp) + "[INFO]`t Renamed $($sam) to $($_.GivenName) $($_.LastName)`r`n" | Out-File $log -append
+			
+			If (($_.CopyGroupMembershipsFrom -ne ""))
+			{
+			Write-Host "Copying group memberships from $($_.CopyGroupMembershipsFrom)`r`n"
+            (insertTimeStamp) + "Copying group memberships from $($_.CopyGroupMembershipsFrom).." | Out-File $log -append
+			Get-ADUser -Identity $_.CopyGroupMembershipsFrom -Properties memberof | Select-Object -ExpandProperty memberof | Add-ADGroupMember -Members $sam
+			}
+		  }
+          Catch
+          {
+            Write-Host "[ERROR]`t Oops, something went wrong: $($_.Exception.Message)`r`n"
+          }
+		  
+		Write-Host "Moving to next iteration`r`n"
+		(insertTimeStamp) + "Moving to next iteration`r`n" | Out-File $log -append
+		$i++
+    }
+  "--------------------------------------------" + "`r`n" | Out-File $log -append
+}
+
+Write-Host "STARTED SCRIPT`r`n"
+Start-Commands
+Write-Host "STOPPED SCRIPT"
