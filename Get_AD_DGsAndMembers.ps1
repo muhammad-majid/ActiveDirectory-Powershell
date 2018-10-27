2e010730f3c6f514e74e869329b2b9d429b5afa0
muhammad-majid <42956457+muhammad-majid@users.noreply.github.com>
2018-10-28 03:19:27 +1100

    Update Get_AD_DGsAndMembers.ps1

---
+++ Get_AD_DGsAndMembers.ps1
@@ -0,0 +1,6 @@
+Import-Module ActiveDirectory
+$Groups = Get-ADGroup -Filter {GroupCategory -eq "Distribution"} -Properties Members
+ForEach ($g in $Groups) {
+    Write-Host $g.name
+    Write-Host $g.members `n
+}
